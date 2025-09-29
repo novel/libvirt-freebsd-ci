050b2db17630bd75db32a56bd1c3fb6806e17002
Roman Bogorodskiy <bogorodskiy@gmail.com>
2025-09-29 19:28:25 +0200

    jenkins: libvirt_tck_git: assorted fixes
     - Fix misc errors and typos in the commands
     - Define threshholds so the job don't fail on skipped tests

--- jenkins/libvirt_tck_git.groovy
+++ jenkins/libvirt_tck_git.groovy
@@ -19,9 +19,9 @@ job('libvirt-tck-git') {
 
     steps {
         shell('''
-sudo bastille destroy -y -a testrunner-git
-sudo bastille create testrunner-git 14.3-RELEASE DHCP virbr0
-sudo bastille template testrunner-git novel/libvirt-tck-git --arg LIBVIRT_TCK_REPO="$LIBVIRT_TCK_REPO" --arg LIBVIRT_TCK_BRANCH="$LIBVIRT_TCK_BRANCH --arg LIBVIRT_REPO="$LIBVIRT_REPO" --arg LIBVIRT_BRANCH="$LIBVIRT_BRANCH"
+sudo bastille destroy -y -a testrunner-git || true
+sudo bastille create -B testrunner-git 14.3-RELEASE DHCP virbr0
+sudo bastille template testrunner-git novel/libvirt-tck-git --arg LIBVIRT_TCK_REPO="$LIBVIRT_TCK_REPO" --arg LIBVIRT_TCK_BRANCH="$LIBVIRT_TCK_BRANCH" --arg LIBVIRT_REPO="$LIBVIRT_REPO" --arg LIBVIRT_BRANCH="$LIBVIRT_BRANCH"
 sudo bastille cmd testrunner-git sh -c "cd /root/libvirt-tck && avocado --config avocado.config  run --xunit ./scripts/domain/*.t ./scripts/storage/*.t ./scripts/networks/*.t ./scripts/hooks/*.t" || true
 sudo bastille rcp testrunner-git /root/avocado/job-results/latest/ $WORKSPACE/test-results
 '''.stripIndent())
@@ -32,6 +32,12 @@ sudo bastille rcp testrunner-git /root/avocado/job-results/latest/ $WORKSPACE/te
         archiveXUnit {
             jUnit {
                pattern('test-results/results.xml')
+            }
+            skippedThresholds {
+                unstable(15)
+                unstableNew(15)
+                failure(15)
+                failureNew(15)
             }
         }
     }
